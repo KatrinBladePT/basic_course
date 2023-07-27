7bacf699f70505119f405e99ff767d8b1a7982fc
KatrinBlade <126530739+KatrinBladePT@users.noreply.github.com>
2023-07-27 09:38:52 +0300

    output

--- task.cs
+++ task.cs
@@ -32,5 +32,17 @@ public class Program
                 count++;
             }
         }
+    string[] outputArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= maxLength)
+            {
+                outputArray[index] = inputArray[i];
+                index++;
+            }
+        }
+
+        return outputArray;
     }
 }
